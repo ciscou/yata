66ad8c3929b13c12b43443d59e67ad4488fd527e
ciscou <francismpp@gmail.com>
2012-03-25 19:10:38 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yata::Application.config.secret_token = 'd84c9b171bae5b6cf93c94e06c484da52a6fc3fe1650fb0c489d57e9adbdfa5bbc11a220dfeb7aca16a4dc09fe538642e5add4e8391a6ade6c51f9e737fd4a6f'
