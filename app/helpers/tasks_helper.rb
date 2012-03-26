3e17fbb70387107d27c94e8fa20829b7f827c8ad
ciscou <francismpp@gmail.com>
2012-03-26 19:01:48 +0200

    refactoring

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,10 @@
 module TasksHelper
+  def link_to_destroy_task(task)
+    icon, text, level = task.done? ? %w[remove Undo danger] : %w[ok Yata success]
+    link_to icon_and_text("icon-white icon-#{icon}", text), task,
+      :remote       => true,
+      :disable_with => "...",
+      :method       => :delete,
+      :class        => "btn btn-#{level} btn-mini"
+  end
 end
