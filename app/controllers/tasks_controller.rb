23264ce456b6d24da4716e81deb12fa9d5a66bb6
ciscou <francismpp@gmail.com>
2012-03-30 23:46:05 +0200

    better ux

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,8 @@
 class TasksController < ApplicationController
   def index
     @show  = params[:show]
-    @show  = "today" unless @show.in? %w[today tomorrow all]
-    @tasks = Task.scoped
-    @tasks = @tasks.todo unless @show == "all"
-    @tasks = @tasks.send(@show)
+    @show  = "today" unless @show.in? %w[today tomorrow todo done]
+    @tasks = Task.send(@show)
   end
 
   def show
@@ -23,7 +21,7 @@ class TasksController < ApplicationController
     @task = Task.new(params[:task])
 
     if @task.save
-      redirect_to @task, notice: 'Task was successfully created.'
+      redirect_to tasks_url(:show => :todo), notice: 'Task was successfully created.'
     else
       render action: "new"
     end
@@ -52,6 +50,6 @@ class TasksController < ApplicationController
   def clear
     Task.done.destroy_all
 
-    redirect_to tasks_path(:show => :all)
+    redirect_to tasks_path(:show => :done)
   end
 end
