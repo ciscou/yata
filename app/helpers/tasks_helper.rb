321cb3aafc65d4e35cc63ac7bd0fd330b488e4ba
ciscou <francismpp@gmail.com>
2012-04-14 17:04:35 +0200

    refactoring views

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -9,6 +9,21 @@ module TasksHelper
       :class        => "btn btn-#{level} #{"btn-mini" unless show}"
   end
 
+  def reminder_beforehand_due_at(task)
+    reminder_beforehand_due_at_options.invert[task.reminder_send_before_due_at] || "Never"
+  end
+
+  def reminder_beforehand_due_at_options
+    {
+      "On time"             => 0,
+      "15 minutes before"   => 15,
+      "Half an hour before" => 30,
+      "One hour before"     => 60,
+      "One day before"      => 60 * 24,
+      "One week before"     => 60 * 24 * 7
+    }
+  end
+
   def time_ago_or_since_in_words(datetime)
     difference = time_ago_in_words datetime
     if datetime.past?
