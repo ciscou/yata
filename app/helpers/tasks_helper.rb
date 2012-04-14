fc13deace3ec6d6919d380f1a467ed1af422e38a
ciscou <francismpp@gmail.com>
2012-04-14 16:42:06 +0200

    refactoring views

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,12 +1,12 @@
 module TasksHelper
-  def link_to_destroy_task(task)
+  def link_to_destroy_task(task, show = false)
     icon, text, level = task.done? ? %w[remove Undo inverse] : %w[ok Yata success]
     level = "danger" if task.delayed?
     link_to icon_and_text("icon-white icon-#{icon}", text), task,
-      :remote       => true,
+      :remote       => !show,
       :disable_with => "...",
       :method       => :delete,
-      :class        => "btn btn-#{level} btn-mini"
+      :class        => "btn btn-#{level} #{"btn-mini" unless show}"
   end
 
   def time_ago_or_since_in_words(datetime)
