66b586ccad70cf95b6632bb03c9eb9be0f98f72a
Francis <francis@aspgems.com>
2012-10-06 23:36:17 +0200

    added some beforehand options

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -19,6 +19,10 @@ module TasksHelper
       "15 minutes before"   => 15,
       "Half an hour before" => 30,
       "One hour before"     => 60,
+      "Two hours before"    => 60 * 2,
+      "Three hours before"  => 60 * 3,
+      "Six hours before"    => 60 * 6,
+      "Twelve hours before" => 60 * 12,
       "One day before"      => 60 * 24,
       "One week before"     => 60 * 24 * 7
     }
