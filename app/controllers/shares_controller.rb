bdf234201acd355b8bc40a409ddc1f7ede156d15
ciscou <francismpp@gmail.com>
2013-08-14 23:13:41 +0200

    share feedback

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -3,6 +3,6 @@ class SharesController < ApplicationController
     @task = current_user.tasks.find(params[:task_id])
     ShareEmail.share_email(@task, params[:email]).deliver
 
-    redirect_to @task
+    redirect_to @task, notice: "Your task was shared successfully!"
   end
 end
