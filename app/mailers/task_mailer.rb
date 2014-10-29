b5a7d7cb5ef82c3b07bbcb738c505a46aed934af
ciscou <francismpp@gmail.com>
2014-10-29 19:20:31 +0100

    one year old typo

--- app/mailers/task_mailer.rb
+++ app/mailers/task_mailer.rb
@@ -1,6 +1,4 @@
 class TaskMailer < ActionMailer::Base
-  helper :tasks
-
   def reminder(task)
     @task = task
     mail(from: "reminders@yata.com", to: task.user.email, subject: "Reminder for task #{task.name.inspect}")
