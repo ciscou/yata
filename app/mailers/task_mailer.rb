a7f9994b5f85964bca10de3803f10f741da8d5e8
ciscou <francismpp@gmail.com>
2014-10-29 19:10:02 +0100

    share to...?

--- app/mailers/task_mailer.rb
+++ app/mailers/task_mailer.rb
@@ -6,7 +6,7 @@ class TaskMailer < ActionMailer::Base
     mail(from: "reminders@yata.com", to: task.user.email, subject: "Reminder for task #{task.name.inspect}")
   end
 
-  def share(task)
+  def share(task, email)
     @task = task
     mail(from: task.user.email, to: email, subject: "I want to share a task with you!")
   end
