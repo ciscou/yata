9cf3a9077c5157c75a7e8593a703c4dae7c312d5
ciscou <francismpp@gmail.com>
2014-11-09 23:03:26 +0100

    an user cannot have two categories with the same name

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,5 +4,5 @@ class Category < ActiveRecord::Base
   belongs_to :user
   has_many :tasks, dependent: :nullify
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: { scope: :user_id, case_sensitive: false }
 end
