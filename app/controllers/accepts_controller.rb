b604cef6df8d2e82b937f9f3259000907d23ad4e
ciscou <francismpp@gmail.com>
2015-02-16 14:06:44 +0100

    cannot accept a share more than twice

--- app/controllers/accepts_controller.rb
+++ app/controllers/accepts_controller.rb
@@ -1,11 +1,22 @@
 class AcceptsController < ApplicationController
+  before_filter :load_task
+  before_filter :cannot_accept_twice
+
   def show
-    @task = Task.find_by_token!(params[:share_id])
   end
 
   def create
-    @task = Task.find_by_token!(params[:share_id])
     current_user.tasks << @task
     redirect_to @task, notice: "Shared task accepted successfully"
+  end
+
+  private
+
+  def load_task
+    @task = Task.find_by_token!(params[:share_id])
+  end
+
+  def cannot_accept_twice
+    redirect_to @task, alert: "You've already accepted that task!" if current_user.tasks.include?(@task)
   end
 end
