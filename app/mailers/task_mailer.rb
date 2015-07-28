d491bcd4254876138390285c19af1c639137b0a1
ciscou <francismpp@gmail.com>
2015-07-28 17:16:30 +0200

    trying to fix emails content-type

--- app/mailers/task_mailer.rb
+++ app/mailers/task_mailer.rb
@@ -3,11 +3,17 @@ class TaskMailer < ActionMailer::Base
 
   def reminder(task)
     @task = task
-    mail(from: "reminders@y-a-t-a.herokuapp.com", to: task.users.map(&:email), subject: "[Yata] #{task.name}")
+
+    mail(from: "reminders@y-a-t-a.herokuapp.com", to: task.users.map(&:email), subject: "[Yata] #{task.name}") do |format|
+      format.html
+    end
   end
 
   def share(task, from, to)
     @task = task
-    mail(from: from, to: to, subject: "[Yata] #{task.name}")
+
+    mail(from: from, to: to, subject: "[Yata] #{task.name}") do |format|
+      format.html
+    end
   end
 end
