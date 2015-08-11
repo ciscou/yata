05ef7c0dd6af838210cdc0b3ead4e74e31ed6afd
ciscou <francismpp@gmail.com>
2015-08-11 23:32:03 +0200

    trying to fix 406 error

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -1,9 +1,5 @@
 class Api::TasksController < ApplicationController
   def index
-    respond_to do |format|
-      format.json do
-        render json: current_user.tasks
-      end
-    end
+    render json: current_user.tasks
   end
 end
