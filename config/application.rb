4cfd1d751abf77ad12853a4c612ddb1a599da162
ciscou <francismpp@gmail.com>
2015-08-15 23:08:54 +0200

    cors patch and put

--- config/application.rb
+++ config/application.rb
@@ -52,7 +52,7 @@ module Yata
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :patch, :put, :options]
       end
     end
   end
